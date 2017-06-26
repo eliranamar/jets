80f890088339c46632f2bb032828c174c8138d42
eliranamar <eliranamar7@gmail.com>
2017-06-26 11:08:59 +0300

    addtakeoff2

--- jet.js
+++ jet.js
@@ -1,18 +1,21 @@
-
 var Jet = function (color, size) {
   this.color = color;
   this.size = size;
 
-  this.tesfunc = function(){
+  this.tesfunc = function () {
     console.log('test');
   }
 
   this.fly = function () {
     console.log('wish');
   }
+  this.takeOff2 = function () {
+    console.log('lift off!');
+  }
   this.takeOff = function () {
     console.log('lift off!');
   }
+
 }
 
 module.exports = Jet;
