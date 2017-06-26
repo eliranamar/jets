db9fd891a8b14143e5a8d8bc2bd670ee0a3e6217
Rami Talisveiber <allindn@gmail.com>
2017-06-26 10:53:25 +0300

    test

--- jet.js
+++ jet.js
@@ -1,11 +1,11 @@
-
+// 
 
 var Jet = function (color, size) {
   this.color = color;
   this.size = size;
 
   this.fly = function () {
-    console.log('whoosh!');
+    console.log('wish');
   }
 }
 
